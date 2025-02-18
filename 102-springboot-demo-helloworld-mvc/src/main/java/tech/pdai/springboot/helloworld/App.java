7b1583921db34f9ec973665bff9308af95602f01
pony <mwsir@qq.com>
2025-02-19 00:14:40 +0800

    add comment

--- 102-springboot-demo-helloworld-mvc/src/main/java/tech/pdai/springboot/helloworld/App.java
+++ 102-springboot-demo-helloworld-mvc/src/main/java/tech/pdai/springboot/helloworld/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
+    // 添加注释
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
